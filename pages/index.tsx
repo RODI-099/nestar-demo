811aa817768bc5965fdd4e2ae7b71f74a43cd016
RODI-099 <rodiiy099@gmail.com>
2024-09-24 20:55:13 +0500

    feat: publish layout components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,23 @@
 import withLayoutMain from "@/libs/components/layout/LayoutHome";
-import { Box, Container, Stack } from "@mui/material";
-import { brown, green } from "@mui/material/colors";
 import { NextPage } from "next";
+import { Stack } from "@mui/material";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import TrendProperties from "@/libs/components/homepage/TrendProperties";
+import PopularProperties from "@/libs/components/homepage/PopularProperties";
+import Advertisement from "@/libs/components/homepage/Advertisement";
+import TopProperties from "@/libs/components/homepage/TopProperties";
+import TopAgents from "@/libs/components/homepage/TopAgents";
 
 const Home: NextPage = () => {
   return (
-    <Stack>
-      <Stack flexDirection={"column"}>
-        <Stack>
-          <Stack className="container">Popular Properties</Stack>
-          </Stack>
-          <Stack>
-          <Stack className="container">Top Agents</Stack>
-          </Stack>
-          <Stack>
-          <Stack className="container">Top Properties</Stack>
-          </Stack>
-      </Stack>
+    <Stack className={"home-page"}>
+      <TrendProperties />
+      <PopularProperties />
+      <Advertisement />
+      <TopProperties />
+      <TopAgents />
     </Stack>
   );
 };
